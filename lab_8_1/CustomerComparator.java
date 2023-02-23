da951e9873821912d999cf40afa89af54c4683c4
Kevin Yu <kevinyukvn@gmail.com>
2023-02-23 17:27:12 +1100

    chore: cleaning up

--- lab_8_1/CustomerComparator.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-class CustomerComparator implements Comparator<Customer> {
-    public int compare(Customer c1, Customer c2) {
-        return c1.getCustomerNumber() - c2.getCustomerNumber() > 0
-                ? -1
-                : 1;
-    }
-}
